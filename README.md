7666eeba7fcefde2c26390abf85ca6d25eb50ae0
Andre Bogus <bogusandre@gmail.com>
2019-03-22 12:52:53 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,53 @@
+# compact_arena
+
+This is a crate with arenas that work with indices. Currently there are two
+types: `SmallArena` uses 32-bit indices and can hold up to 2³² objects.
+`TinyArena` uses 16-bit indices and can hold up to 65535 objects, regardless
+of object size.
+
+This can conserve memory in scenarios where we have a large-ish number of
+objects that need to relate to each other, e.g. in graph algorithms.
+
+## Usage:
+
+Add the following dependency to your `Cargo.toml`
+
+```
+compact_arena = "0.0.1"
+```
+
+By default, the `TinyArena` uses no unsafe code to maintain storage, but
+requires the stored types to be `Default + Copy`. To change this, you can use
+the `uninit` feature to enable usage on all types with a bit more unsafe code:
+
+```
+compact_arena = { version = "0.0.1", features = ["uninit"] }
+```
+
+In your code, you use it as follows:
+
+```
+compact_arena::in_arena(|arena| {
+    let hello = arena.add("Hello");
+    let world = arena.add("World");
+    println!("{}, {}!", arena[hello], arena[world]);
+}
+```
+
+For further information, please read the [documentation](https://docs.rs/compact_arena).
+
+# License
+
+Licensed under either of
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally
+submitted for inclusion in the work by you, as defined in the Apache-2.0
+license, shall be dual licensed as above, without any additional terms or
+conditions.
---
+++ src/lib.rs
@@ -0,0 +1,295 @@
+#![deny(missing_docs)]
+
+//! A crate with a few single-typed arenas that work exclusively with indexes.
+//! The indexes are sized with the arena. This can reduce memory footprint when
+//! changing pointers to indices e.g. on 64-bit systems.
+//!
+//! The arenas use "branded indices": The indices contain lifetimes that bind
+//! them to the arena so you cannot mix up two arenas by accident. See
+//! [Gankro's thesis](https://github.com/Gankro/thesis/blob/master/thesis.pdf)
+//! for more information about the concept and it's implementation in Rust.
+//!
+//! Use the `in_arena` and similar methods to run code within the scope of an
+//! arena:
+//!
+//! # Examples
+//!
+//! ```rust
+//! compact_arena::in_arena(|arena| {
+//!     let idx = arena.add(1usize);
+//!     assert_eq!(1, arena[idx]);
+//! });
+//! ```
+//!
+//! You can use `in_sized_arena` to change the initial size of the backing
+//! memory:
+//!
+//! ```rust
+//!# #[allow(dead_code)]
+//! compact_arena::in_sized_arena(1, |arena| {
+//!     ..
+//!# ; arena.add(1usize);
+//! });
+//! ```
+//!
+//! You can nest calls to work with multiple arenas:
+//!
+//! ```rust
+//!# #[allow(dead_code)]
+//! compact_arena::in_sized_arena(1, |a| {
+//!     compact_arena::in_sized_arena(1, |b| {
+//!         ..
+//!# ; a.add(1u32); b.add(1u32);
+//!     })
+//! });
+//! ```
+//!
+//! The compiler will give you a scary lifetime error if you mix up arenas:
+//!
+//! ```rust,compile_fail
+//! compact_arena::in_sized_arena(8, |a| {
+//!     compact_arena::in_sized_arena(8, |b| {
+//!         let i = a.add(1usize);
+//!         b[i]
+//!     }
+//! }
+//! ```
+
+use core::ops::{Index, IndexMut};
+use core::default::Default;
+use core::marker::PhantomData;
+
+#[derive(Copy, Clone, PartialEq, PartialOrd, Eq)]
+struct Id<'id>(PhantomData<*mut &'id ()>);
+
+unsafe impl Sync for Id<'_> {}
+unsafe impl Send for Id<'_> {}
+
+impl<'id> Default for Id<'id> {
+    #[inline]
+    fn default() -> Self { Id(PhantomData) }
+}
+
+/// An index into the arena
+///
+/// The only way to get an index into an arena is to `add` a value to it. With
+/// an `Idx` you can index or mutably index into the arena to observe or mutate
+/// the value.
+#[derive(Copy, Clone, PartialOrd, PartialEq, Eq)]
+pub struct Idx<'id, I> {
+    id: Id<'id>,
+    index: I,
+}
+
+/// The index type for a small arena is 32 bits large
+///
+/// # Examples
+///
+/// ```rust
+///# use core::mem::size_of;
+///# use compact_arena::Idx32;
+/// assert_eq!(size_of::<Idx32<'_>>(), size_of::<u32>());
+/// ```
+pub type Idx32<'id> = Idx<'id, u32>;
+
+/// A "Small" arena based on a resizable slice (i.e. a `Vec`) that can be
+/// indexed with 32-bit `Idx32`s. This can help reduce memory overhead when
+/// using many pointer-heavy objects on 64-bit systems.
+///
+/// You will usually use this type by calling `in_arena` or similar functions.
+pub struct SmallArena<'id, T> {
+    id: Id<'id>,
+    // TODO: Use a custom structure, forbid resizing over 2G items
+    data: Vec<T>,
+}
+
+const INITIAL_CAPACITY: u32 = 1024 * 1024; // start with 1M elements
+
+/// Run code using an arena. The indirection through a `FnOnce` is required
+/// to bind the indices to the arena.
+#[inline]
+pub fn in_arena<T, F: for<'t> FnOnce(&mut SmallArena<'t, T>) -> O, O>(f: F) -> O {
+    in_sized_arena(INITIAL_CAPACITY, f)
+}
+
+/// Same as `with`, but allows specifying the initial size of the arena.
+#[inline]
+pub fn in_sized_arena<T, F, O>(size: u32, f: F) -> O
+where F: for<'t> FnOnce(&mut SmallArena<'t, T>) -> O {
+    f(&mut SmallArena {
+        id: Id::default(),
+        data: Vec::with_capacity(size as usize),
+    })
+}
+
+
+impl<'id, T> SmallArena<'id, T> {
+    /// Add an item to the arena, get an index back
+    #[inline]
+    pub fn add(&mut self, item: T) -> Idx32<'id> {
+        let i = self.data.len() as u32;
+        self.data.push(item);
+        Idx { id: self.id, index: i }
+    }
+}
+
+impl<'id, T> Index<Idx32<'id>> for SmallArena<'id, T> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, i: Idx32<'id>) -> &T {
+        unsafe { &self.data.get_unchecked(i.index as usize) }
+    }
+}
+
+impl<'id, T> IndexMut<Idx32<'id>> for SmallArena<'id, T> {
+    #[inline]
+    fn index_mut(&mut self, i: Idx32<'id>) -> &mut T {
+        unsafe { self.data.get_unchecked_mut(i.index as usize) }
+    }
+}
+
+const TINY_ARENA_ITEMS: usize = 65535;
+
+/// The index type for a tiny arena is 16 bits large
+///
+/// # Examples:
+///
+/// ```rust
+///# use compact_arena::Idx16;
+///# use core::mem::size_of;
+/// assert_eq!(size_of::<Idx16<'_>>(), size_of::<u16>());
+/// ```
+pub type Idx16<'id> = Idx<'id, u16>;
+
+pub use tiny_arena::{in_tiny_arena, TinyArena};
+
+#[cfg(not(feature = "uninit"))]
+mod tiny_arena {
+    use crate::{Id, Idx16, TINY_ARENA_ITEMS};
+    use core::ops::{Index, IndexMut};
+
+    /// Run code using a tiny arena. The indirection through a `FnOnce` is
+    /// required to bind the indices to the arena. This version only works
+    /// for types that implement `Default` and `Copy`. You can use the `uninit`
+    /// feature to remove that restriction, at the cost of some unsafe code.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// compact_arena::in_tiny_arena(|arena| {
+    ///     let idx = arena.add(1usize);
+    ///     assert_eq!(1, arena[idx]);
+    /// });
+    /// ```
+    #[inline]
+    pub fn in_tiny_arena<F, O, T: Default + Copy>(f: F) -> O
+    where F: for<'t> FnOnce(&mut TinyArena<'t, T>) -> O {
+        f(&mut TinyArena {
+            id: Id::default(),
+            data: [Default::default(); TINY_ARENA_ITEMS],
+            len: 0,
+        })
+    }
+
+    /// A "tiny" arena containing <64K elements. This variant only works with
+    /// types implementing `Default`.
+    ///
+    /// You will likely use this via the `in_tiny_arena` function.
+    pub struct TinyArena<'id, T> {
+        id: Id<'id>,
+        len: u16,
+        data: [T; TINY_ARENA_ITEMS],
+    }
+
+    impl<'id, T> TinyArena<'id, T> {
+        /// Add an item to the arena, get an index back
+        pub fn add(&mut self, item: T) -> Idx16<'id> {
+            let i = self.len;
+            assert!((i as usize) < TINY_ARENA_ITEMS);
+            self.data[i as usize] = item;
+            self.len += 1;
+            Idx16 { id: self.id, index: i }
+        }
+    }
+
+    impl<'id, T> Index<Idx16<'id>> for TinyArena<'id, T> {
+        type Output = T;
+        fn index(&self, i: Idx16<'id>) -> &T {
+            &self.data[i.index as usize]
+        }
+    }
+
+    impl<'id, T> IndexMut<Idx16<'id>> for TinyArena<'id, T> {
+        fn index_mut(&mut self, i: Idx16<'id>) -> &mut T {
+            &mut self.data[i.index as usize]
+        }
+    }
+}
+
+#[cfg(feature = "uninit")]
+mod tiny_arena {
+    use crate::{Id, Idx16, TINY_ARENA_ITEMS};
+    use core::mem::{self, ManuallyDrop};
+    use core::ops::{Index, IndexMut};
+
+    /// Run code using an arena. The indirection through a `FnOnce` is
+    /// required to bind the indices to the arena.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// compact_arena::in_tiny_arena(|arena| {
+    ///     let idx = arena.add(1usize);
+    ///     assert_eq!(1, arena[idx]);
+    /// });
+    /// ```
+    #[inline]
+    pub fn in_tiny_arena<T, F: for<'t> FnOnce(&mut TinyArena<'t, T>) -> O, O>(f: F) -> O {
+        assert!(mem::size_of::<T>() > 0, "zero-sized types are not allowed");
+        f(&mut TinyArena {
+            id: Id::default(),
+            data: unsafe { mem::uninitialized() },
+            len: 0,
+        })
+    }
+
+    /// A "tiny" arena containing <64K elements.
+    pub struct TinyArena<'id, T> {
+        id: Id<'id>,
+        len: u16,
+        data: [ManuallyDrop<T>; TINY_ARENA_ITEMS],
+    }
+
+    impl<'id, T> TinyArena<'id, T> {
+        /// Add an item to the arena, get an index back
+        pub fn add(&mut self, item: T) -> Idx16<'id> {
+            let i = self.len;
+            self.data[i as usize] = ManuallyDrop::new(item);
+            self.len += 1;
+            Idx16 { id: self.id, index: i }
+        }
+
+        /// dropping the arena drops all values
+        pub fn drop(&mut self) {
+            for i in 0..self.len as usize {
+                unsafe { ManuallyDrop::drop(&mut self.data[i]) };
+            }
+            self.len = 0;
+        }
+    }
+
+    impl<'id, T> Index<Idx16<'id>> for TinyArena<'id, T> {
+        type Output = T;
+        fn index(&self, i: Idx16<'id>) -> &T {
+            &self.data[i.index as usize]
+        }
+    }
+
+    impl<'id, T> IndexMut<Idx16<'id>> for TinyArena<'id, T> {
+        fn index_mut(&mut self, i: Idx16<'id>) -> &mut T {
+            &mut self.data[i.index as usize]
+        }
+    }
+}
+
